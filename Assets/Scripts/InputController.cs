aeecf0c68fbf47247c21f8af84e0066aa1e2712c
John Tuttle <jtuttle@craw.cc>
2017-01-21 18:18:48 -0500

    fix input controller, rotate base

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
+  public GameObject Base;
+
+  private Vector2 _basePos;
+
   void Start() {
-    
+    _basePos = Camera.main.WorldToScreenPoint(Base.transform.position);
   }
 	
   void Update() {
-    Vector3 mousePos = Input.mousePosition;
-    Debug.Log(mousePos);
+    FollowMouse();
+  }
+
+  private void FollowMouse() {
+    Vector2 dir = (Vector2)Input.mousePosition - _basePos;
+    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    Base.transform.rotation = rotation;
   }
 }
