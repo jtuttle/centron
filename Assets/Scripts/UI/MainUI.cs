fd2ff1472857f8dbb9acbb7490f3769af330a86c
Isaiah Mann <contact@isaiahmann.com>
2017-01-21 18:15:17 -0500

    Adding bars to UI

---
+++ Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour 
+{
+  [SerializeField]
+  // Use this for initialization
+  void Start() 
+  {
+   
+  }
+
+}
