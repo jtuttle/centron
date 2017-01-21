cd7af0e8445519f4c9903890f1126efde47fcce3
John Tuttle <jtuttle@craw.cc>
2017-01-21 18:36:47 -0500

    move mouselook code, set up events

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WaveType {
+  Low, High
+}
+
 public class Base : MonoBehaviour {
   public int Health = 100;
   public float Cooldown = 100;
--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,38 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
-  public GameObject Base;
-
-  private Vector2 _basePos;
-
-  void Start() {
-    _basePos = Camera.main.WorldToScreenPoint(Base.transform.position);
-  }
-	
   void Update() {
-    FollowMouse();
-
     if(Input.GetMouseButtonDown(0)) {
       ShootWave();
     }
 
     if(Input.GetMouseButtonDown(1)) {
-      SwitchWave();
+      SwitchWaveType();
     }
   }
 
-  private void FollowMouse() {
-    Vector2 dir = (Vector2)Input.mousePosition - _basePos;
-    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    Base.transform.rotation = rotation;
-  }
-
   private void ShootWave() {
-    Debug.Log("PEW!");
+    //EventModule.Event("ShootWave");
   }
 
-  private void SwitchWave() {
-    Debug.Log("SWITCH!");
+  private void SwitchWaveType() {
+    //EventModule.Event("SwitchWaveType");
   }
 }
