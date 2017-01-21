2fe2a36e827531838b4ce93077f0b7456f9250f0
Isaiah Mann <contact@isaiahmann.com>
2017-01-21 17:26:31 -0500

    Adding tuning file and parsing

---
+++ Centron/Assets/Scripts/Testing/TuningTest.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuningTest : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start() 
+	{
+		Tuning tuning = Tuning.Get;
+		Debug.Log(tuning.MaxPlayerBaseHealth);
+		Debug.Log(tuning.HighPitchAttackDistance);
+	}
+
+}
---
+++ Centron/Assets/Scripts/Tuning.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Loads in tuning variables
+ * Usage: Singleton pattern
+ */
+
+using UnityEngine;
+
+[System.Serializable]
+public class Tuning 
+{
+	public static Tuning Get {
+		get {
+			if(!isInitialized)
+			{
+				_instance = init();
+			}
+			return _instance;
+		}
+	}
+
+	static Tuning _instance;
+	static bool isInitialized = false;
+
+	static Tuning init() {
+		string json = Resources.Load<TextAsset>("JSON/Tuning").text;
+		Tuning tuning = JsonUtility.FromJson<Tuning>(json);
+		isInitialized = true;
+		return tuning;
+	}
+		
+	public int MaxPlayerBaseHealth;
+	public float PlayerWaveAttacksPerCooldown;
+	public float PlayerAttackCooldown;
+	public float UnitSpawnFrequency;
+	public float EnemyMovementRate;
+	public float HighPitchAttackDistance;
+}
