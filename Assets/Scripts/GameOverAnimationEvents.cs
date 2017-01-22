803a03a9900e9ee55413c24e5f60bbb59beda021
John Tuttle <jtuttle@craw.cc>
2017-01-22 13:45:50 -0500

    remove planet

--- Assets/Scripts/GameOverAnimationEvents.cs
+++ Assets/Scripts/GameOverAnimationEvents.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverAnimationEnd : MonoBehaviour {
+public class GameOverAnimationEvents : MonoBehaviour {
+  public void OnPlanetExplode() {
+    GameObject planet = GameObject.Find("Planet");
+    GameObject.Destroy(planet);
+  }
+
   public void OnAnimationEnd() {
     EventModule.Event(EventType.GAME_OVER_ANIMATION_COMPLETE);
   }
