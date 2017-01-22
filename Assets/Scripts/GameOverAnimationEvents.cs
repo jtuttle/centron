a09f36ef3c9523f185fe372ee74cae7a41df68c5
John Tuttle <jtuttle@craw.cc>
2017-01-22 15:03:28 -0500

    remove base on death

--- Assets/Scripts/GameOverAnimationEvents.cs
+++ Assets/Scripts/GameOverAnimationEvents.cs
@@ -4,6 +4,9 @@ public class GameOverAnimationEvents : MonoBehaviour {
   public void OnPlanetExplode() {
     GameObject planet = GameObject.Find("Planet");
     GameObject.Destroy(planet);
+
+    GameObject playerBase = GameObject.Find("PlayerBase");
+    GameObject.Destroy(playerBase);
   }
 
   public void OnAnimationEnd() {
