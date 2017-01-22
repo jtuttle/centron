4abc0cb3536ecd0346a872c181f07534a118f6fa
Isaiah Mann <contact@isaiahmann.com>
2017-01-22 03:19:34 -0500

    Adding separate cooldown bars and logic

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
-  private bool _shootWaveEnabled = true;
+  private bool _highWaveReady = true;
+  private bool _lowWaveReady = true;
+  [SerializeField]
+  PlayerBase player;
+
   [SerializeField] KeyCode switchWaveKey = KeyCode.Space;
   public enum InputEvent {
     ShootWave, SwitchWaveType
@@ -18,24 +22,38 @@ public class InputController : MonoBehaviour {
   }
 
   void Update() {
-    if(_shootWaveEnabled && Input.GetMouseButtonDown(0)) {
-      ShootWave();
+    if(canShootCurrentWave() && Input.GetMouseButtonDown(0)) {
+      ShootWave(player.CurrentWaveType);
     }
-
     if(Input.GetMouseButtonDown(1) || Input.GetKeyDown(switchWaveKey)) {
       SwitchWaveType();
     }
   }
 
+  bool canShootCurrentWave() {
+    return (_highWaveReady && player.CurrentWaveType == WaveType.High) ||
+      (_lowWaveReady && player.CurrentWaveType == WaveType.Low);
+  }
+
   public void OnEvent(string eventType) {
-    if(eventType == EventType.WAVE_SHOOT_READY) {
-      _shootWaveEnabled = true;
+    if(eventType == EventType.HIGH_WAVE_READY) {
+      _highWaveReady = true;
+    } else if (eventType == EventType.LOW_WAVE_READY) {
+      _lowWaveReady = true;
     }
   }
 
-  private void ShootWave() {
-    _shootWaveEnabled = false;
-    EventModule.Event(EventType.SHOOT_WAVE);
+  private void ShootWave(WaveType waveType) {
+    switch(waveType) {
+      case WaveType.High:
+        _highWaveReady = false;
+        EventModule.Event(EventType.SHOOT_HIGH_WAVE);
+        break;
+      case WaveType.Low:
+        _lowWaveReady = false;
+        EventModule.Event(EventType.SHOOT_LOW_WAVE);
+        break;
+    }
   }
 
   private void SwitchWaveType() {
